54bded0a01b8d6cb59dc36b2d77c1ffb58ce94e1
David Wilkie <dwilkie@gmail.com>
2025-11-13 22:54:58 +0700

    chore(main): release 0.1.1 (#10)
    * chore(main): release 0.1.1
    
    ---------
    
    Co-authored-by: Samnang Chhun <samnang.chhun@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "org.somleng.sms_gateway_app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "0.1.0" // x-release-please-version
+    versionCode = 2
+    versionName = "0.1.1" // x-release-please-version
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
