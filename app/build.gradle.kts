867083bc994fa86659176ded4cf617de11c78139
David Wilkie <dwilkie@gmail.com>
2025-11-15 14:49:04 +0700

    chore(main): release 0.2.0 (#18)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "org.somleng.sms_gateway_app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
-    versionName = "0.1.1" // x-release-please-version
+    versionCode = 3
+    versionName = "0.2.0" // x-release-please-version
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
