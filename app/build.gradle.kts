ac8940ff01c13bf16fe14bc488e534c97c81cba6
Samnang Chhun <samnang.chhun@gmail.com>
2025-11-19 22:25:46 +0700

    Fix: network security for production release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "org.somleng.sms_gateway_app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 6
-    versionName = "0.4.1" // x-release-please-version
+    versionCode = 7
+    versionName = "0.4.2" // x-release-please-version
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
